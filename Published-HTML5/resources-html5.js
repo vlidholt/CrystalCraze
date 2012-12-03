c8a095d38662b287a6953b34beb77821c9650f44
Viktor Lidholt <vlidholt@zynga.com>
2012-12-02 20:31:58 -0800

    Initial commit

---
+++ Published-HTML5/resources-html5.js
@@ -0,0 +1,17 @@
+var ccb_resources = [
+    {type:'ccbi', src:"MainScene.ccbi"},
+    {type:'image', src:"gems/2.png"},
+    {type:'plist', src:"ccbResources/ccbDefaultImages.plist"},
+    {type:'image', src:"ccbResources/ccbParticleSmoke.png"},
+    {type:'image', src:"ccbResources/ccbDefaultImages.png"},
+    {type:'image', src:"gems/1.png"},
+    {type:'image', src:"ccbResources/ccbParticleFire.png"},
+    {type:'image', src:"gems/4.png"},
+    {type:'image', src:"clickme.png"},
+    {type:'image', src:"ccbResources/ccbParticleStars.png"},
+    {type:'image', src:"gems/0.png"},
+    {type:'ccbi', src:"GameScene.ccbi"},
+    {type:'image', src:"gems/3.png"},
+    {type:'image', src:"clickme-down.png"},
+    {type:'image', src:"ccbResources/ccbParticleSnow.png"},
+];
